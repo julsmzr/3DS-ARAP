38c99b4649bea0c1a36fffce5d5b70c8be77ece3
Kamber Vogli <kambervogli@gmail.com>
2025-06-27 21:40:01 +0200

    reindent

--- src/solver.h
+++ src/solver.h
@@ -5,10 +5,10 @@
 #include <Eigen/Dense>
 
 namespace Solver {
-
-Eigen::Vector3d screenToWorld(
-    const glm::vec2&   screenCoords,
-    const Eigen::Vector3d& planePoint,
-    const glm::vec3&   planeNormal);
+    Eigen::Vector3d screenToWorld(
+        const glm::vec2&   screenCoords,
+        const Eigen::Vector3d& planePoint,
+        const glm::vec3&   planeNormal
+    );
 
 } // namespace Solver
